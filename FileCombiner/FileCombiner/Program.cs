c086e42f3f0c64c5b32e10af4002b9e17b9ec0a9
rolywhateva <roli25veres@gmail.com>
2019-03-21 15:39:26 +0200

    dsfsdfsdfsdf

---
+++ FileCombiner/FileCombiner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileCombiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           string[] files =  Directory.GetFiles(Directory.GetCurrentDirectory(), "*.in");
+            Console.WriteLine(files.Length);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            StreamWriter writer = new StreamWriter("date.out");
+            foreach(string file in files)
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                   // Console.WriteLine("*");
+                    string[] words = file.Split(new char[] { '\\', ':', 'C', 'D' }, StringSplitOptions.RemoveEmptyEntries);
+                    writer.WriteLine(words[words.Length-1]);
+
+                    writer.WriteLine(reader.ReadLine());
+                }
+            }
+            writer.Close();
+            Console.ReadKey();
+            
+        }
+    }
+}
